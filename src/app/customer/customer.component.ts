9f1fcc96dbf0d0b6df573ac25c0a0a1e81d62c35
Ray Felton <frajeon@gmail.com>
2019-11-27 10:01:17 -0500

    y

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from '../services/customer.service';
 import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
-import { observable, Observable } from 'rxjs';
+import {  Observable } from 'rxjs';
 
 @Component({
   selector: 'app-customer',
@@ -33,13 +33,15 @@ export class CustomerComponent implements OnInit {
 submitCustomer(){
   if (this.customerform.valid){
     this.validMessage = "Your Customer Form Has Been Submitted. Thank You!";
-    this.customerService.createCustomer(this.customerform.value).subscribe(data => {this.customerform.reset();
-    return true; 
-  }),
+    this.customerService.createCustomer(this.customerform.value).subscribe(data => {
+      console.log(data);
+      this.customerform.reset();
+      return true; 
+  },
   error => {
     Observable.throw(error);
-
-  }
+    }
+    )
 
   } else {
     this.validMessage = "Please Fill Out The Form Before Submitting!";
